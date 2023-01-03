f9ff36e8951b14174e63a2804a3d20a9926a6607
norenz92 <51136661+norenz92@users.noreply.github.com>
2023-01-03 14:27:21 +0100

    Update tibber-prices.js

--- tibber-prices.js
+++ tibber-prices.js
@@ -1,4 +1,3 @@
 http_data = http_data.data.viewer.homes[0].currentSubscription.priceInfo.today.map((item) => item.total)
-const tomorrowValues = http_data.data.viewer.homes[0].currentSubscription.priceInfo.tomorrow.map((item) => item.total)
 
 return http_data
