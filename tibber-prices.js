ca02ca6de8c3381b25f51b693e986d0ccf3a1897
norenz92 <51136661+norenz92@users.noreply.github.com>
2023-01-03 14:41:44 +0100

    Update tibber-prices.js

--- tibber-prices.js
+++ tibber-prices.js
@@ -1,2 +1,2 @@
 var parsed_data = http_data.data.viewer.homes[0].currentSubscription.priceInfo.today.map((item) => item.total)
-var string_value = parsed_data.join(",")
+setGlobal("parsed_data", parsed_data.join(","));
